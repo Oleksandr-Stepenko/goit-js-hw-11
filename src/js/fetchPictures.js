d9253fe7a7448cdac7047a00fcecfd951a625a4a
Oleksandr-Stepenko <113541212+Oleksandr-Stepenko@users.noreply.github.com>
2023-03-24 13:08:15 +0200

    add 11 hw

---
+++ src/js/fetchPictures.js
@@ -0,0 +1,51 @@
+// import axios from 'axios';
+
+// export default async function fetchPictures(searchQuery, page) {
+//   const BASE_URL = 'https://pixabay.com/api/';
+//   const KEY = '33815884-23ed77a9da78718943d0614f4';
+//   try {
+//     const response = await axios.get(
+//       `${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+//     );
+//     console.log(response.data);
+//     return response.data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+
+import axios from 'axios';
+
+export default class newApiImageService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchImage() {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const KEY = '33815884-23ed77a9da78718943d0614f4';
+
+    const response = await axios.get(
+      `${BASE_URL}?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
+    );
+    return response.data;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    return this.searchQuery = newQuery;
+  }
+};
---
+++ src/js/pictures-service.js
@@ -0,0 +1,13 @@
+// export default class PicApiService {
+// 	constructor() {		
+//     this.searchQuery = '';
+//     this.page = 1;
+//     }
+//   get query() {
+//         return this.searchQuery;
+//     }
+//   set query(newQuery) {
+//         this.searchQuery = newQuery;
+//     }
+	
+// }
